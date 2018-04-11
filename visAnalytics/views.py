8cae4dad7a988f3212605d12452b6e6c7dc82141
Jose Septien <jaseptienh@gmail.com>
2018-04-11 16:37:33 -0500

    Add the view for the histogram.

--- visAnalytics/views.py
+++ visAnalytics/views.py
@@ -1,4 +1,65 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+from handlers import histogram as h
+
+# Create your views here.
+def HistogramHandler( request ):
+    """
+    The histogram handler on the server. This handler will be in charge of calculating the
+    frequencies and all other data that the client requiers for displaying the histogram.
+    The data are recieve via the query string. The parameters needed are:
+        -The database (db) on which the analysis is being done.
+        -The axis on which the histogram is to be calculated.
+        -If the html template is requiered.
+    The function will return to the client (in a JSON object), the following data:
+        -HTML (if required).
+        -Name of the fieldset of the html (if requiered).
+        -X-axis range ([a, b]).
+        -Minimum and maximum of the frequencies ([0, maxFreq]).
+        -Name of the axis (if available).
+        -Array of the frequencies.
+        -Default number of classes (bins).
+        -Width of each class.
+    """
+    # Get all the data
+    dbName = request.GET.get('db')
+    axis = int(request.GET.get('axis'))
+    needHtml = bool(request.GET.get('needhtml'))
+
+    # Por el momento, supondremos que la base de datos es un archivo
+
+    # Compute the histogram
+    hist = h.Histograma()
+    hist.loadDataFromFile(dbName)
+    hist.computeHistogram()
+    # Get the corresponding data
+    frequencies = hist.getFrequencies()
+    xAxisRange = hist.getXAxisRange()
+    (numBins, binWidth, minFreq, maxFreq) = hist.getHistogramData()
+    n = hist.getNumData()
+
+    template = ""
+    histogramid = "histogramplot" + str(axis)
+    if needhtml:
+        context = {
+            histogramid: histogramid,
+            minB: 1,
+            maxB: n,
+            numbins: numBins
+        }
+        template = render_to_string("histogramTemplate.html", context)
+    
+    json = {
+        html: template,
+        xRange: xAxisRange,
+        minF: minFreq,
+        maxF: maxFreq,
+        freqs: frequencies,
+        binWidth: binWidth,
+        numbins: numBins,
+        histogramid: histogramid
+    }
+
+    return JsonResponse(json)
 
