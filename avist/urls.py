e02ed8710835970c9ba593fd50adb98e7c267c03
Jose Septien <jaseptienh@gmail.com>
2018-04-12 17:06:01 -0500

    Add handler for static files. Redirect / to /visAnalytics.

--- avist/urls.py
+++ avist/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Add the urls from the visAnalytics app
     path('visAnalytics/', include('visAnalytics.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', RedirectView.as_view(url="/visAnalytics/")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
