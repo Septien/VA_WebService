741b6fb56ddcd329b2f567733e2df00a105b9cca
Jose Septien <jaseptienh@gmail.com>
2018-04-12 17:07:03 -0500

    Add url for index page.

--- visAnalytics/urls.py
+++ visAnalytics/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     #https://stackoverflow.com/questions/3711349/django-and-query-string-parameters
+    # The main entry point
+    path('', views.index),
     # For the histogram
     path(r"histogram", views.HistogramHandler),
 ]
